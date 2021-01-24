7b9640fcb86e275ed9719cbb2b0ce6c7926f50c9
Adrian Crainic <adrian.crainic@bgasoft.net>
2021-01-24 14:13:24 +0200

    First Commit for ChatApp

---
+++ app/Http/Controllers/ChatController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\ChatRoom;
+use App\Models\ChatMessage;
+use Illuminate\Support\Facades\Auth;
+use App\Events\NewChatMessage;
+
+class ChatController extends Controller
+{
+    //return all rooms
+    public function rooms(Request $request) {
+        return ChatRoom::all();
+    }
+    //return all messages for a room
+    public function messages(Request $request, $roomId) {
+        return ChatMessage::where('chat_room_id',$roomId)
+            ->with('user')
+            ->orderBy('created_at','DESC')
+            ->get();
+    }
+
+    public function newMessage(Request $request, $roomId){
+        $newMessage = new ChatMessage;
+        $newMessage->user_id = Auth::id();
+        $newMessage->chat_room_id = $roomId;
+        $newMessage->message = $request->message;
+        $newMessage->save();
+
+        broadcast(new NewChatMessage( $newMessage))->toOthers(); //send events to another people(not current user)
+        return $newMessage;
+    }
+}
---
+++ resources/js/Pages/Chat/container.vue
@@ -0,0 +1,98 @@
+<template>
+    <app-layout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                <chat-room-selection
+                    v-if="currentRoom.id"
+                    :rooms="chatRooms"
+                    :currentRoom="currentRoom"
+                    v-on:roomchanged="setRoom( $event )"
+                />
+            </h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                   <message-container :messages="messages" />
+                    <input-message :room="currentRoom"
+                    v-on:messagesent="getMessages()"/>
+                </div>
+            </div>
+        </div>
+    </app-layout>
+</template>
+
+<script>
+    import AppLayout from '@/Layouts/AppLayout'
+    import Welcome from '@/Jetstream/Welcome'
+    import MessageContainer from "@/Pages/Chat/messageContainer";
+    import InputMessage from "@/Pages/Chat/inputMessage";
+    import ChatRoomSelection from "@/Pages/Chat/chatRoomSelection";
+
+    export default {
+        components: {
+            ChatRoomSelection,
+            InputMessage,
+            MessageContainer,
+            AppLayout
+
+        },
+        data: function () {
+            return {
+                chatRooms: [],
+                currentRoom: [],
+                messages: []
+            }
+        },
+        watch: {
+            currentRoom(val, oldVal) {
+                if (oldVal.id){
+                    this.disconnect(oldVal);
+                }
+                this.connect();
+            }
+        },
+        methods: {
+            getRooms() {
+                axios.get('/chat/rooms')
+                .then( response => {
+                    this.chatRooms = response.data;
+                    this.setRoom(response.data[0]);
+                })
+                .catch( error => {
+                    console.log(error);
+                })
+            },
+            setRoom (room) {
+                this.currentRoom = room;
+            },
+            getMessages(){
+                axios.get('/chat/room/' + this.currentRoom.id + '/messages')
+                .then( response => {
+                    this.messages = response.data;
+                })
+                .catch(error =>{
+                    console.log(error);
+                })
+            },
+            connect() {
+                if(this.currentRoom.id) {
+                    let vm = this;
+                    this.getMessages();
+                    window.Echo.private('chat.' + this.currentRoom.id)
+                            .listen('.message.new', e => {
+                                vm.getMessages();
+                            });
+                }
+            },
+            disconnect( room ) {
+                window.Echo.leave("chat." + room.id);
+            }
+        },
+        created() {
+            this.getRooms();
+        }
+    }
+</script>
+<!--https://youtu.be/CkRGJC0ytdU?t=1776-->
